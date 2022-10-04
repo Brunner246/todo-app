02b1f8ce773bd5ed2331b1ad52dcb1d1f5472b67
Merlin Unterfinger <info@munterfinger.ch>
2022-10-04 22:09:35 +0200

    Proposal for a layout.
    The layout is based on the Generic Business Application with a toggle show/hide menu.

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,81 @@
 import './App.css'
 
+function toggleMenu() {
+  const menu = document.getElementById('menu')
+  if (menu !== null) {
+    if (menu.style.width === '0px') {
+      menu.style.width = '170px'
+    } else {
+      menu.style.width = '0px'
+    }
+  }
+}
+
 export const App = () => {
-  return <div>{/* TOOD: implement App */}</div>
+  return (
+    <div className="container shadow">
+      <div className="header shadow">
+        Todo App
+        <span className="toggle clickable" onClick={toggleMenu}>
+          &#8801;
+        </span>
+      </div>
+      <div className="main">
+        <div className="todo create">
+          <input className="input" type="text" placeholder="Enter a task..." />
+          <input className="button" type="button" value="Add" />
+          <div className="options">
+            <input type="checkbox" id="show-all" name="showAll" value="false" />
+            <label>Show all</label>
+          </div>
+        </div>
+        <div className="todo list">
+          <div className="todo item">
+            <input type="checkbox" />
+            <div className="priority">
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+            </div>
+            <span>My first task</span>
+            <input className="button" type="button" value="Delete" />
+          </div>
+          <div className="todo item">
+            <input type="checkbox" />
+            <div className="priority">
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+            </div>
+            <span>My second task</span>
+            <input className="button" type="button" value="Delete" />
+          </div>
+          <div className="todo item">
+            <input type="checkbox" />
+            <div className="priority">
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+              <span className="clickable">&#128498;</span>
+            </div>
+            <span>My third task</span>
+            <input className="button" type="button" value="Delete" />
+          </div>
+        </div>
+      </div>
+      <div id="menu" className="menu">
+        <div className="settings">
+          <div>Account</div>
+          <hr />
+          <input className="button" type="button" value="Logout" />
+          <br />
+          <br />
+          <div>Settings</div>
+          <hr />
+          <input className="button" type="button" value="Import" />
+          <input className="button" type="button" value="Export" />
+        </div>
+      </div>
+      <div className="footer">© Group 1</div>
+    </div>
+  )
 }
